166bd6974455f8b0bd3bf726013d227a9147bc1e
HMAsp <hanand55935@stud.noroff.no>
2024-05-29 09:52:24 +0200

    feat: Adds logo export handler

---
+++ src/assets/logos/logoExports.js
@@ -0,0 +1,43 @@
+import bootstrapLogo from "./icons8-bootstrap-logo.svg";
+import cssLogo from "./icons8-css3.svg";
+import htmlLogo from "./icons8-html-5.svg";
+import jsLogo from "./icons8-javascript-logo.svg";
+import reactLogo from "./icons8-react.svg";
+import sassLogo from "./sass-logo-7702.svg";
+import figmaLogo from "./icons8-figma.svg";
+import gitLogo from "./icons8-git-logo.svg";
+import githubLogo from "./icons8-github.svg";
+import nodeLogo from "./nodejsStackedLight.svg";
+import npmLogo from "./npm-logo-red.svg";
+import discordJsLogo from "./icons8-discord-js.svg";
+import wordpressLogo from "./icons8-wordpress.svg";
+import cypressLogo from "./cypress_idJT_qtr1C_1716966855208.svg";
+import jestLogo from "./jest-svgrepo-com.svg";
+import tailwindLogo from "./icons8-tailwind-css.svg";
+import viteLogo from "./icons8-vite-logo.svg";
+import vscLogo from "./icons8-visual-studio-code-2019.svg";
+import flutterLogo from "./icons8-flutter.svg";
+import spookyLogo from "./Spookyrumble_text.png";
+
+export {
+  bootstrapLogo,
+  cssLogo,
+  htmlLogo,
+  jsLogo,
+  reactLogo,
+  sassLogo,
+  figmaLogo,
+  gitLogo,
+  githubLogo,
+  nodeLogo,
+  npmLogo,
+  discordJsLogo,
+  wordpressLogo,
+  cypressLogo,
+  jestLogo,
+  tailwindLogo,
+  viteLogo,
+  vscLogo,
+  flutterLogo,
+  spookyLogo,
+};
