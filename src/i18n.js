62c73deb79fc8c5814d0d537c984bd09ff621fa9
HMAsp <hanand55935@stud.noroff.no>
2024-06-02 13:40:27 +0200

    chore: Update i18n resources for English and Norwegian translations

--- src/i18n.js
+++ src/i18n.js
@@ -3,24 +3,14 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const resources = {
-  en: {
-    translation: "/public/locales/en/translation.json",
-  },
-  no: {
-    translation: "/public/locales/no/translation.json",
-  },
-};
-
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
     fallbackLng: "en",
     backend: {
-      loadPath: "/public/locales/{{lng}}/{{ns}}.json",
+      loadPath: "../public/locales/{{lng}}/{{ns}}.json",
     },
     interpolation: {
       escapeValue: false,
