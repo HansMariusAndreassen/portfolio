44b1fe7eb2278cb5f6895e5fa774f0a0416e1042
HMAsp <hanand55935@stud.noroff.no>
2024-06-04 14:11:51 +0200

    refactor: Update profile image and add styling to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import profileImage from "../../assets/profile/selfie_cv.png";
+import profileImage from "../../assets/profile/selfie_cv.jpg";
 
 const Home = () => {
   const { t } = useTranslation();
@@ -14,6 +14,7 @@ const Home = () => {
         src={profileImage}
         alt="Profile"
         className="mb-8 rounded-sm border-slateBlue border-t-4 border-l-4 shadow-lg"
+        style={{ height: "300", width: "300px" }}
       />
       <div className="text-center p-8 rounded-lg">
         <h1 className="text-4xl font-bold mb-2">{t("thankYou")}</h1>
